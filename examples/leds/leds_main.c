b64245ace64b18685001d0f4235b3ebc5fc5a6d1
Richard Tucker <richardmtucker85@gmail.com>
2023-11-06 09:06:12 +1100

    apps/examples/leds: turn LEDs off on exit

--- examples/leds/leds_main.c
+++ examples/leds/leds_main.c
@@ -187,7 +187,6 @@ static int led_daemon(int argc, char *argv[])
         }
 
       ledset = newset;
-      printf("led_daemon: LED set 0x%02x\n", (unsigned int)ledset);
 
       ret = ioctl(fd, ULEDIOC_SETALL, ledset);
       if (ret < 0)
@@ -205,9 +204,14 @@ static int led_daemon(int argc, char *argv[])
    * task terminated by a SIGTERM
    */
 
+  ledset = 0;
+  ioctl(fd, ULEDIOC_SETALL, ledset);
+  close(fd);
   exit(EXIT_SUCCESS);
 
 errout_with_fd:
+  ledset = 0;
+  ioctl(fd, ULEDIOC_SETALL, ledset);
   close(fd);
 
 errout:
